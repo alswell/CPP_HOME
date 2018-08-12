aa09e83df957228593bfa42ece8cabbb52c491d9
alswell <icemanzhouning@yeah.net>
2018-08-12 13:07:04 +0800

    modify jpg.cpp

--- CppUtils/jpg.h
+++ CppUtils/jpg.h
@@ -1,11 +1,9 @@
-#ifndef __JPG_H
-#define __JPG_H
-#include "afx.h"
+#pragma once
+
+#include "env.h"
 
 #include "jpeglib.h"
 #include "jerror.h"
 
 int jpeg2rgb(unsigned char* buff, unsigned size, unsigned char*& rdata, unsigned& w, unsigned& h);
 
-
-#endif
