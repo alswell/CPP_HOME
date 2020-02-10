bc3489b9518b0a2b9db15501370d2a3bc81c3d91
alswell <icemanzhouning@yeah.net>
2020-02-10 17:51:53 +0800

    read write png file

--- basic/jpg.h
+++ basic/jpg.h
@@ -5,7 +5,12 @@
 #include "jpeglib.h"
 #include "jerror.h"
 
+#include "png.h"
+
 int jpeg2rgb(unsigned char* buff, unsigned size, unsigned char*& img, unsigned& w, unsigned& h, unsigned scale_denom = 1);
 int jpeg2rgb(const char* filename, unsigned char*& img, unsigned& w, unsigned& h, unsigned scale_denom = 1);
 
 void rgb2jpeg(const char* filename, unsigned char* img, unsigned w, unsigned h);
+
+bool png2rgb(const char* filename, unsigned char*& img, unsigned& w, unsigned& h);
+void rgb2png(const char* filename, unsigned char* img, unsigned w, unsigned h);
